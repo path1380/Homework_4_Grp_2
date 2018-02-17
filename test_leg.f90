0f23735a2e91c65d2d337013b04f3c1d29d706ef
Fortino Garcia <fortino.garcia@colorado.edu>
2018-02-17 12:52:29 -0700

    Updated leg_funs and test routine to verify correctness of Legendre derivatives

--- test_leg.f90
+++ test_leg.f90
@@ -6,13 +6,34 @@ program test_leg
     use type_defs
     use leg_funs
     implicit none
-    integer, parameter :: q = 5
-    real(dp) :: x
+
+    integer, parameter :: q = 4, alpha = 0, beta = 0, n = 10
+    real(dp) :: x(0:n-1), stepsize, poly(0:q), polyder(0:q), errvec(0:n), trueder(0:q)
+    integer :: i
     real(dp), dimension(0:q) :: leg_arr
 
-    x = 0.5_dp
-    leg_arr = leg(q, x)
+    trueder(0) = 0.0_dp
+    !x = 0.5_dp
+    stepsize = 2.0_dp/dble(n-1)
 
-    write(*,*) leg_arr
+    do i = 0, n-1
+    	x(i) = -1.0_dp + stepsize*i
+    end do
+  	
+  	!Call routine to output values of derivatives of Legendre poly's
+  	!and compare with true derivative values.
+    do i =0, n-1
+   		poly = jacobiP(q,x(i),alpha+1,beta+1)
+   		polyder = gradjacobiP(q,x(i),alpha,beta,poly)
+
+   		trueder(1) = 1
+   		trueder(2) = 3.0_dp*x(i)
+   		trueder(3) = 0.5_dp*(15.0_dp*(x(i)**2.0_dp) - 3.0_dp)
+   		trueder(4) = 17.5_dp*x(i)**3.0_dp - 7.5_dp*x(i)
+
+   		errvec(i) = MAXVAL(ABS(trueder - polyder))
+    end do
+
+    write(*,*) MAXVAL(errvec)
 
 end program test_leg
